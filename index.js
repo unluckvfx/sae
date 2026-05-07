2c12c8559cd52d00a1dbb08e66b569fde2baa583
unluckvfx <unluckvfx@gmail.com>
2026-05-07 17:30:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 export default {
   async fetch(request, env) {
 
-    // добавляем запись
-    if (request.method === "POST") {
-      await env.DB.prepare(
-        "INSERT INTO messages (text) VALUES (?)"
-      ).bind("hello from worker").run();
+    await env.DB.prepare(
+      "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)"
+    ).run();
 
-      return new Response("Inserted!");
-    }
+    await env.DB.prepare(
+      "INSERT INTO messages (text) VALUES (?)"
+    ).bind("test message").run();
 
-    // читаем базу
     const { results } = await env.DB.prepare(
       "SELECT * FROM messages ORDER BY id DESC"
     ).all();
 
-    return new Response(
-      JSON.stringify(results, null, 2),
-      {
-        headers: {
-          "content-type": "application/json"
-        }
-      }
-    );
+    return new Response(JSON.stringify(results, null, 2), {
+      headers: { "content-type": "application/json" }
+    });
   }
 }
